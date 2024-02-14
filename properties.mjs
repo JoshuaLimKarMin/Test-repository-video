70bd8e664945c133f7c7542eb7e86d075217f6d3
JoshuaLimKarMin <joshua.lim.kar.min@gmail.com>
2024-02-15 00:28:33 +0800

    Comment removed

--- properties.mjs
+++ properties.mjs
@@ -20,21 +20,6 @@ await new Promise(res => setTimeout(() => {
    res()
 }, 3000))
 
-// OLD CODE -- Start
-
-
-// const mainContentElement = ".x9f619.x19h7ccj"
-// await page.waitForSelector(mainContentElement)
-// const mainContentSelecorHandle = await(await page.$(mainContentElement)).$('div')
-// const mainContent = await page.evaluate(element => element.innerHTML, mainContentSelecorHandle)
-
-// await mainContentSelecorHandle.dispose()
-
-// writeFileSync('./test.html', mainContent)
-
-
-// OLD CODE -- End
-
 const postsElementSelector = ".x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z"
 const postMessageElementSelector = ".x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x3x7a5m.x6prxxf.xvq8zen.xo1l8bm.xzsf02u.x1yc453h"
 const seeMoreButtonSelector = "[role=\"button\"].x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.xt0b8zv.xzsf02u.x1s688f"
